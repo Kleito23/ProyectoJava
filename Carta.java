e946b01586136e93a8562a1c2962ee30fe046800
HenryOlvera28 <henrymoises2815@gmail.com>
2024-10-02 14:11:30 -0500

    Add files via upload
    public si

---
+++ Carta.java
@@ -0,0 +1,45 @@
+package ec.edu.espol.Clases;
+
+import java.util.ArrayList;
+
+public abstract class Carta {
+    enum Color {R,A,V,Z,N}
+    private static ArrayList<String> comodines;
+    protected Color color;
+    protected String valor;
+
+    protected Carta(Color color, String valor) {
+        this.color = color;
+        this.valor = valor;
+        comodines = new ArrayList<>();
+        setComodines();
+    }
+
+    public Color getColor() {
+        return color;
+    }
+    public String getColorStr(){
+        return color.toString();
+    }
+    public String getValor() {
+        return valor;
+    }
+
+    private void setComodines(){
+        comodines.add("^");
+        comodines.add("&");
+        comodines.add("%");
+        comodines.add("+4");
+        comodines.add("+2");
+    }
+
+    public static ArrayList<String> getComodines() {
+        return comodines;
+    }
+
+    @Override
+    public String toString() {
+        return "("+ color;
+    }
+
+}
